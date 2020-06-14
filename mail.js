50c8fcc3d0ec276a1a556f0f8163ced7918dc4e2
Diego Essence <diego.quaresma@assessoriaessence.com.br>
2020-06-14 14:46:50 -0300

    Correção da ordem dos nome e empresa

--- mail.js
+++ mail.js
@@ -24,9 +24,9 @@ const transporter = nodemailer.createTransport({
 
 var x = "comercial@assessoriaessence.com.br"
 
-const sendMail = (email, subject, text, name, empresa, cb) => {
+const sendMail = (email, subject, text, empresa, name, cb) => {
     const mailOptions = {
-        from: 'Contato através site <contato.site@assessoriaessence.com.br>', // TODO replace this with your own email
+        from: `${name} - Contato Através Site <contato.site@assessoriaessence.com.br>`, // TODO replace this with your own email
         to: "comercial@assessoriaessence.com.br",   // TODO: the receiver email has to be authorized for the free tier
         subject,
         html: `Nome: ${name} <br> Empresa: ${empresa} <br> e-mail: ${email} <br> <br> ${text}`,     
