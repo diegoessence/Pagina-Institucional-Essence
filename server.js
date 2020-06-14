c7786ec600bc25a40c1fe56c60b96bedf0dc858d
Diego Essence <diego.quaresma@assessoriaessence.com.br>
2020-06-13 23:22:23 -0300

    Index

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.post('/email', (req, res) => {
 
 // Render home page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, '', 'index.html'));
 });
 
 // Error page
